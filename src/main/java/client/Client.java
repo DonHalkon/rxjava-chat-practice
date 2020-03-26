eae3a9f3961c03a916aade560281c21f49c32aea
Sviatoslav Naiden <sv.nayden@gmail.com>
2020-03-26 12:02:08 +0100

    basic RXJava for Client

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,6 +1,8 @@
 package client;
 
-import java.io.File;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -15,46 +17,37 @@ public class Client {
     private static List<String> messages;
     private static int length;
 
-    public static void main(String[] args) {
-        String username = args.length > 0 ? args[0] : "";
-        try (Socket socket = new Socket("localhost", 8080)) {
+    static {
+        try {
             messages = Files.readAllLines(Paths.get(Client.class.getClassLoader().getResource("manual.txt").getPath()));
             length = messages.size();
-
-            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
-            printWriter.println(username);
-            new Thread(() -> {
-                //for manual input
-
-//                try (Scanner userInput = new Scanner(System.in)) {
-//                    while (userInput.hasNextLine()) {
-//                        final String message = userInput.nextLine();
-//                        Thread.sleep(r.nextInt(5000)); // slow network imitation stub
-//                        printWriter.println(message);
-//                    }
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-                while (true) {
-                    try {
-                        Thread.sleep(r.nextInt(10));
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    printWriter.println(messages.get(r.nextInt(length)));
-                }
-            }).start();
-
-            try (Scanner scanner = new Scanner(socket.getInputStream())) {
-                String line = null;
-                while (scanner.hasNextLine()) {
-//                    line = scanner.nextLine();
-                    System.out.println(scanner.nextLine());
-                }
-//                System.out.println(line);
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        String username = args.length > 0 ? args[0] : "";
+        try (Socket socket = new Socket("localhost", 8080)) {
+
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            printWriter.println(username);
+            Scanner userInput = new Scanner(System.in);
+            Observable
+                    .fromStream(userInput.tokens())
+                    .observeOn(Schedulers.io())
+                    .subscribe(printWriter::println, Throwable::printStackTrace);
+            Runtime.getRuntime().addShutdownHook(new Thread(userInput::close));
+
+            try (Scanner serverResponses = new Scanner(socket.getInputStream())) {
+                serverResponses.useDelimiter("\n");
+                Observable.fromStream(serverResponses.tokens()).subscribe(System.out::println);
+//                    serverResponses.tokens().forEach(System.out::println);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
